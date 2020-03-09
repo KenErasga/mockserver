ea4a04f5913820afae3515fde0ec11e872d29103
Aparna <aparna.das@coveainsurance.co.uk>
2020-03-09 15:31:40 +0000

    new changes

--- Mock/automationTest/support/config/config.ts
+++
@@ -1,5 +0,0 @@
-export default {
-hostEnv: process.env.RISK_ROOT_URL || "https://int.gateway.integration-dev.digital.coveahosted.co.uk",
-authUrl: process.env.AUTH_URL || "https://login.microsoftonline.com/fdb0e18a-61f3-49e2-891f-ce2def987b59/oauth2/token",
-authClientId: process.env.CLIENT_ID || 'caff42d8-4290-4f85-a5ba-a75f423cca88'
-};
